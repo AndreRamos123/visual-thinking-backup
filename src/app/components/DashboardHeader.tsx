df7bffcc29b394fc0b21977ad6674f14d8496021
AndreRamos123 <andrecardosoramos2001@gmail.com>
2025-12-02 23:29:21 +0000

    Melhoria: Implementar debounce na barra de pesquisa #3

--- src/app/components/DashboardHeader.tsx
+++ src/app/components/DashboardHeader.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 
+import { useEffect, useState } from "react";
 interface DashboardHeaderProps {
   searchQuery: string;
   setSearchQuery: (query: string) => void;
 }
 
+const DELAY = 500;
+
 export default function DashboardHeader({
   searchQuery,
   setSearchQuery,
 }: DashboardHeaderProps) {
+  const [value, setValue] = useState(searchQuery);
+  const [debouncedValue, setDebouncedValue] = useState("");
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, DELAY);
+
+    return () => clearTimeout(handler);
+  }, [value]);
+
+  useEffect(() => {
+    if (debouncedValue) {
+      setSearchQuery(debouncedValue);
+    }
+  }, [debouncedValue]);
+
   return (
     <div className="border-b border-[#e2e8f0] p-4 flex justify-between items-center bg-white">
       <h2 className="text-2xl font-bold tracking-tight text-slate-900">
@@ -18,8 +38,8 @@ export default function DashboardHeader({
         <input
           className="flex h-10 w-[400px] rounded-md border border-black bg-transparent px-3 py-2 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 disabled:cursor-not-allowed disabled:opacity-50"
           placeholder="Server-side search..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         />
         <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-slate-900 text-slate-50 hover:bg-slate-900/90 h-10 px-4 py-2">
           Export Data
